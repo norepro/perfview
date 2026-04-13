e30957762d4b697b50059dff4ca4b58365a67493
Will Sheehan <wshee@microsoft.com>
2026-04-13 13:53:17 -0700

    Remove Views and VIewModels folders to align with PerfView

--- src/PerfView.Avalonia/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PerfView.Avalonia.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- src/PerfView/MainWindow.xaml.cs
+++ src/PerfView/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 #else
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 #endif
 
 namespace PerfView
